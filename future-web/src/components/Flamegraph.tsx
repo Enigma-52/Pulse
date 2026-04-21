60429c855cc05c58984ff8618c205341cc12878c
Rohit Singh <rohit.singh.33521@gmail.com>
2026-04-21 15:02:34 +0530

    core web refactor and improvements

--- future-web/src/components/Flamegraph.tsx
+++ future-web/src/components/Flamegraph.tsx
@@ -48,7 +48,11 @@ export default function Flamegraph({ spans, total }: Props) {
   const toggle = (id: string) =>
     setCollapsed(prev => {
       const next = new Set(prev);
-      next.has(id) ? next.delete(id) : next.add(id);
+      if (next.has(id)) {
+        next.delete(id);
+      } else {
+        next.add(id);
+      }
       return next;
     });
 
